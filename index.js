b184480d567eb40cb836739a74ffc578a5ab0808
Hongarc <anhhong12121997t@gmail.com>
2018-01-16 11:24:26 +0700

    Add web dyno

--- index.js
+++ index.js
@@ -2,6 +2,25 @@
 let api = require('./src/api');
 const fs = require('fs');
 
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.writeHead(200, {
+        'Content-Type': 'text/json; charset=utf-8'
+    });
+    let date = new Date();
+    date.setUTCHours(date.getUTCHours() + /*VN timeZone */ 7 + date.getTimezoneOffset() / 60);
+    let nowTime = date.getHours() + ":" + date.getMinutes();
+    let data = {
+        status: "App is running",
+        time: nowTime
+    };
+    res.end(JSON.stringify(data, null, 4));
+});
+// app.use(express.static('public'));
+let port = process.env.PORT || 1997;
+app.listen(port, () => console.log("This app is running in Port: ", port));
 
 let loadFeed = () => {
 
